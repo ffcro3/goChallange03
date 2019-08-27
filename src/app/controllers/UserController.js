ffda0212cb5188e07b6b38552feef498991a0342
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-26 21:07:03 -0300

    chore: User Creation + Check email before create

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,6 +3,14 @@ import User from '../models/User';
 class UserController {
   //  METHOD TO CREATE A NEW USER
   async store(req, res) {
+    const UserExists = req.body.email;
+
+    if (UserExists) {
+      return res.status(400).json({
+        error: 'User Already exists',
+      });
+    }
+
     const { name, email, password, userType } = await User.create(req.body);
 
     return res.json({
