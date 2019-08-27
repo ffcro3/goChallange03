7c903470e4b841feff24979d5cea03a117a1b920
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-27 20:22:29 -0300

    chore: User update. Auth Middlware.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -24,6 +24,48 @@ class UserController {
       userType,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    // FIND THE USER TO MAKE THE ID AVAILABLE TO UPDATE
+    const user = await User.findByPk(req.userId);
+
+    // CHECK IF USER IS CHANGING THE E-MAIL FOR ONE THAT ALREADY EXISTS
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (userExists) {
+        return res.status(400).json({
+          error: 'User already exists',
+        });
+      }
+    }
+
+    // CHECK IF USER IS TRYINT TO CHANGE PASSWORD AND THEN HE MUST CONFIRM THE
+    // OLD PASSWORD THAT ALREADY IS REGISTRED
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        error: 'Password does not match',
+      });
+    }
+
+    const { id, name, userType, imagemUsuario, active } = await user.update(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      userType,
+      imagemUsuario,
+      active,
+    });
+  }
 }
 
 export default new UserController();
